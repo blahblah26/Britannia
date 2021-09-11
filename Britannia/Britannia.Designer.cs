91b50d93dd7c30c39201dafcba919c03ccf3d5ff
Bob Joe <texanguy62@gmail.com>
2021-09-11 18:10:52 -0500

    Creation, basic exploring

---
+++ Britannia/Britannia.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace Britannia
+{
+    partial class Britannia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTest = new System.Windows.Forms.Label();
+            this.lblBruh = new System.Windows.Forms.Label();
+            this.cmbTest = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Location = new System.Drawing.Point(259, 92);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(35, 13);
+            this.lblTest.TabIndex = 0;
+            this.lblTest.Text = "label1";
+            this.lblTest.Click += new System.EventHandler(this.lblTest_Click);
+            // 
+            // lblBruh
+            // 
+            this.lblBruh.AutoSize = true;
+            this.lblBruh.Location = new System.Drawing.Point(508, 178);
+            this.lblBruh.Name = "lblBruh";
+            this.lblBruh.Size = new System.Drawing.Size(35, 13);
+            this.lblBruh.TabIndex = 1;
+            this.lblBruh.Text = "label2";
+            // 
+            // cmbTest
+            // 
+            this.cmbTest.FormattingEnabled = true;
+            this.cmbTest.Items.AddRange(new object[] {
+            "Lmao",
+            "Lmao XD",
+            "BRUH",
+            "HAHA"});
+            this.cmbTest.Location = new System.Drawing.Point(172, 169);
+            this.cmbTest.Name = "cmbTest";
+            this.cmbTest.Size = new System.Drawing.Size(121, 21);
+            this.cmbTest.TabIndex = 2;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(685, 210);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(685, 234);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Britannia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.cmbTest);
+            this.Controls.Add(this.lblBruh);
+            this.Controls.Add(this.lblTest);
+            this.Name = "Britannia";
+            this.Text = "Britannia";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.Label lblBruh;
+        private System.Windows.Forms.ComboBox cmbTest;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Britannia/Britannia.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Britannia
+{
+    public partial class Britannia : Form
+    {
+        public Britannia()
+        {
+            InitializeComponent();
+            InitComponents();
+        }
+
+        private void InitComponents()
+        {
+            lblTest.Text = "This is cool";
+            lblTest.Size = new Size(500, 500);
+            lblTest.Visible = true;
+            lblTest.ForeColor = Color.Firebrick;
+            lblTest.Enabled = true;
+            lblTest.BackColor = Color.Aqua;
+            lblTest.Refresh();
+            ArrayList ar = new ArrayList();
+            for (int i = 0; i < 10; i++)
+            {
+                ar.Add(i);
+            }
+            cmbTest.DataSource = ar;
+        }
+
+        private void lblTest_Click(object sender, EventArgs e)
+        {
+            lblTest.Text = "clicked!";
+            lblTest.Size = new Size(1000, 1000);
+            lblTest.Font = new Font(FontFamily.GenericMonospace, 30);
+            lblTest.Refresh();
+        }
+    }
+}
